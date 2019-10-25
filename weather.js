3b108e4a55e4b8085c625796b98b4c991413dd3a
Hannah0124 <hannahjoo24@gmail.com>
2019-10-25 00:53:44 -0700

    Fetch Weather from API

--- weather.js
+++ weather.js
@@ -1,1 +1,24 @@
- 
+class Weather {
+  constructor(city, country) {
+    this.apiKey = 'd07f906a4da48e94608362cc317ea0fd';
+    this.city = city;
+    this.country = country;
+  }
+
+  // Fetch weather from API
+  async getWeather() {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&appid=${this.apiKey}`);
+
+    // http://api.openweathermap.org/data/2.5/weather?q=boston,us&appid=d07f906a4da48e94608362cc317ea0fd
+
+    const responseData = await response.json();
+
+    return responseData;
+  }
+
+  // Change weather location 
+  changeLocation(city, country) {
+    this.city = city;
+    this.country = country;
+  }
+}
